cf85620bfba4e8c82821d9295212987e11c55b9e
faisalbazid <91886338+faisalbazid@users.noreply.github.com>
2022-01-05 19:46:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ function checkEmptyInput()
     auteur = document.getElementById("auteur").value;
     prix =   document.getElementById("prix").value;
     date =   document.getElementById("date").value;
+    email =   document.getElementById("email").value;
     lang =   document.getElementById("lang").value;        
     typedouvrage = document.querySelector('input[name="typeO"]:checked').value;
 
-
+   
 
     if(titre === ""){
         alert("'TITRE'  can not be empty");
@@ -28,6 +29,10 @@ function checkEmptyInput()
         alert("'DATE'  can not be empty");
         isEmpty = true;
     }
+    else if(email === ""){
+        alert("EMAIL  can not be empty");
+        isEmpty = true;
+    }
     else if(lang === ""){
         alert("'LANGUE'  can not be empty");
         isEmpty = true;
@@ -37,10 +42,137 @@ function checkEmptyInput()
         isEmpty = true;
     }
     return isEmpty;
+    
 }
-function AddTableRow()
-{
-    if(!checkEmptyInput()){
+
+    function sortTable() {
+        var table, i, x, y;
+        table = document.getElementById("table");
+        var switching = true;
+
+        
+        while (switching) {
+            switching = false;
+            var rows = table.rows;
+
+           
+            for (i = 1; i < (rows.length - 1); i++) {
+                var Switch = false;
+
+                
+                x = rows[i].getElementsByTagName("td")[0];
+                y = rows[i + 1].getElementsByTagName("td")[0];
+
+               
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
+                    {
+
+                
+                    Switch = true;
+                    break;
+                }
+            }
+            if (Switch) {
+               
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
+        }
+    }
+      
+  
+
+    function prixCheck() {
+
+    var Price = new RegExp(/^\d+(,\d{1,2})?$/);
+    var prix = document.getElementById("prix").value;
+    var valid = Price.test(prix);
+
+    if (valid == true)
+    {
+    console.log("The Price is Valid")
+    }
+
+    else {
+    alert("The Price is not Valid")
+
+    }
+
+    }
+
+        function dateCheck() {
+
+        var datee = new RegExp(/(?:((0\d|[12]\d|3[01])|(0\d|1[012]))[\-|\\|\/]((0\d|1[012])|(0\d|[12]\d|3[01]))[\-|\\|\/](((19|20)\d{2})|\d\d))/);
+        var checkDate = document.getElementById("date").value;
+        var validDate = datee.test(checkDate)
+
+        if (validDate==true)
+        {
+        console.log("The Date is valid")
+        }
+
+        }
+
+        
+
+            function emailCheck() {
+
+            
+
+            let emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+            let checkEmail = document.getElementById("email").value;
+            let validate = emailRegex.test(checkEmail);
+
+            if(validate == true) {
+            console.log("Email is valid")
+            return true;
+
+            } else {
+            alert("Email is not valid");    
+            }
+
+            }
+        
+
+    function localstorage(){
+        
+            titre.value,
+            auteur.value,
+            prix.value,
+            date.value,
+            email.value,
+            lang.value,        
+            typedouvrage.value,
+        
+        
+        
+        localStorage.setItem("titre",document.getElementById("titre").value);
+        localStorage.setItem("auteur",document.getElementById("auteur").value);
+        localStorage.setItem("titre",document.getElementById("prix").value);
+        localStorage.setItem("date",document.getElementById("date").value);
+        localStorage.setItem("email",document.getElementById("email").value);
+        localStorage.setItem("lang",document.getElementById("lang").value);
+        localStorage.setItem("typedouvrage",document.querySelector('input[name="typeO"]:checked').value);
+    
+    
+};
+
+   
+
+     function Print(){
+    var btn = document.getElementById("print");
+    btn.addEventListener("click", Print);
+        window.print(), id="noprint"
+
+    }
+    
+            
+        
+   
+
+function AddTableRow(){
+   
+    if(!checkEmptyInput() && emailCheck()){
    var newRow = table.insertRow(table.length);
 
     var cell1 = newRow.insertCell(0);
@@ -49,11 +181,13 @@ function AddTableRow()
     var cell4 = newRow.insertCell(3);
     var cell5 = newRow.insertCell(4);
     var cell6 = newRow.insertCell(5);
+    var cell7 = newRow.insertCell(6);
 
     titre =  document.getElementById("titre").value;
     auteur = document.getElementById("auteur").value;
     prix =   document.getElementById("prix").value;
     date =   document.getElementById("date").value;
+    email =   document.getElementById("email").value;
     lang =   document.getElementById("lang").value;        
     typedouvrage = document.querySelector('input[name="typeO"]:checked').value;
 
@@ -61,12 +195,68 @@ function AddTableRow()
     cell2.innerHTML= auteur;
     cell3.innerHTML= prix;
     cell4.innerHTML= date;
-    cell5.innerHTML= lang;
-    cell6.innerHTML= typedouvrage;
+    cell5.innerHTML= email;
+    cell6.innerHTML= lang;
+    cell7.innerHTML= typedouvrage;
 
-    SelectRowtoInput();}
 
-}
+    showAlert(Ouvrage);
+    prixCheck();
+    dateCheck();
+    // emailCheck();
+    localstorage();
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+}                      //fin de fonction add
+
+   
+   
+        
+
+
+
+function showAlert(Ouvrage) {
+let OuvrageOne = new Ouvrage();
+alert(OuvrageOne.detailOuvrage()); 
+
+                            }
+
+class Ouvrage {
+
+    constructor() {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.prix = prix;
+        this.date = date;
+        this.email = email;
+        this.lang = lang;
+        this.typedouvrage = typedouvrage;
+                }
+
+    detailOuvrage() {
+        
+    return        `L ouvrage ${this.titre} est un ${this.typedouvrage}; en langue ${this.lang},
+    écrit par ${this.auteur} et publié le ${this.date}; Le prix de ${this.titre} est de ${this.prix} Dhs.`
+                    }
+                    
+
+            }   
+    
+ 
+
+SelectRowtoInput();   
+
+
+
+
 
 function SelectRowtoInput()
 {
@@ -81,8 +271,9 @@ function SelectRowtoInput()
             document.getElementById("auteur").value=this.cells[1].innerHTML;
             document.getElementById("prix").value=this.cells[2].innerHTML;
             document.getElementById("date").value=this.cells[3].innerHTML;
-            document.getElementById("lang").value=this.cells[4].innerHTML;
-            document.querySelector('input[name="typeO"]:checked').value=this.cells[5].innerHTML;
+            document.getElementById("email").value=this.cells[4].innerHTML;
+            document.getElementById("lang").value=this.cells[5].innerHTML;
+            document.querySelector('input[name="typeO"]:checked').value=this.cells[6].innerHTML;
         }
     }
 
@@ -95,6 +286,7 @@ function EditTableSelectedRow(){
     var auteur= document.getElementById("auteur").value;
     var prix = document.getElementById("prix").value;
     var date= document.getElementById("date").value;
+    var email= document.getElementById("email").value;
     var lang =document.getElementById("lang").value;
     var typedouvrage =document.querySelector('input[name="typeO"]:checked').value;
 
@@ -104,8 +296,9 @@ table.rows[rIndex].cells[0].innerHTML=titre;
 table.rows[rIndex].cells[1].innerHTML=auteur;
 table.rows[rIndex].cells[2].innerHTML=prix;
 table.rows[rIndex].cells[3].innerHTML=date;
-table.rows[rIndex].cells[4].innerHTML=lang;
-table.rows[rIndex].cells[5].innerHTML=typedouvrage;
+table.rows[rIndex].cells[4].innerHTML=email;
+table.rows[rIndex].cells[5].innerHTML=lang;
+table.rows[rIndex].cells[6].innerHTML=typedouvrage;
 }
 
 }
@@ -120,5 +313,3 @@ function DelTableSelectdRow()
     document.getElementById("lang").value = "";
     document.querySelector('input[name="typeO"]:checked').value = "";
 }
-
-
